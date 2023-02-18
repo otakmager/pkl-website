99cb30d675a60cbea33ab060462781f923e23a83
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-02-18 10:19:08 +0700

    Change size button in makun

--- resources/views/dashboard/fetch/makun-data.blade.php
+++ resources/views/dashboard/fetch/makun-data.blade.php
@@ -7,18 +7,18 @@
         @if ($user->level === 'pimpinan')
             Aktif
         @else
-            <select name="status" id="status" class="form-control" data-id="{{ $user->id }}" style="min-width: 120px">
-                <option value="0" @if ($user->status === 0) selected @endif>Non Aktif</option>
+            <select name="status" id="status" class="form-control" data-id="{{ $user->id }}">
+                <option value="0" @if ($user->status === 0) selected @endif>Non-aktif</option>
                 <option value="1" @if ($user->status === 1) selected @endif>Aktif</option>
             </select>
         @endif
     </td>
     <td class="text-center">
         @if ($user->level === 'pimpinan')
-            <a href="javascript:void(0)" id="btn-setting-admin" data-id="{{ $user->id }}" class="btn btn-icon icon-left btn-warning"><i class="fas fa-screwdriver-wrench"></i> Pengaturan Akun</a>
+            <a href="javascript:void(0)" id="btn-setting-admin" data-id="{{ $user->id }}" class="btn btn-icon icon-left btn-warning btn-sm"><i class="fas fa-screwdriver-wrench"></i> Pengaturan Akun</a>
         @else
-            <a href="javascript:void(0)" id="btn-reset-user" data-id="{{ $user->id }}" class="btn btn-icon icon-left btn-warning" ><i class="fas fa-key"></i> Reset Password</a>
-            <a href="javascript:void(0)" id="btn-del-user" data-id="{{ $user->id }}" class="btn btn-icon icon-left btn-danger" ><i class="fas fa-trash"></i> Hapus</a>        
+            <a href="javascript:void(0)" id="btn-reset-user" data-id="{{ $user->id }}" class="btn btn-icon icon-left btn-warning btn-sm" ><i class="fas fa-key"></i> Reset Pass</a>
+            <a href="javascript:void(0)" id="btn-del-user" data-id="{{ $user->id }}" class="btn btn-icon icon-left btn-danger btn-sm"><i class="fas fa-trash"></i> Hapus</a>        
         @endif   
     </td>
     </tr>
