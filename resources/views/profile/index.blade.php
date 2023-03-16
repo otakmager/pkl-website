e384d6e255f7586bc506106612aa5cdf22e272fe
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-03-16 18:14:17 +0700

    Add modal profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,6 +4,12 @@
 
 @push('style')
     <!-- CSS Libraries -->
+    <style>
+        .mylabel {
+            color: #000000;
+            font-size: 14pt;
+        }
+    </style>
 @endpush
 
 @section('main')
@@ -15,17 +21,60 @@
             <div class="section-body">
                 <div class="card">
                     <div class="card-body">
-                        <h3 style="color: #1a1c2b">Hi, Selamat Datang {{ Auth::check() ? auth()->user()->name : 'UserNotLoginYet' }}!</h3>
-                        <p class="mt-3 text-justify" style="color:#111111">Di aplikasi ini, Anda dapat menginputkan 
-                            <a href="{{ url('tmasuk') }}">transaksi masuk</a> dan <a href="{{ url('tkeluar') }}">transaksi keluar</a> 
-                            sesuai format formulir yang disediakan. Jika Anda tidak sengaja atau ingin memulihkan data yang dihapus, 
-                            Anda dapat pergi ke menu <a href="{{ url('sampah') }}">Sampah</a> 
-                            selama data tersebut belum 14 hari sejak dihapus.</p>
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 col-12 text-center mb-3">
+                                <img alt="image"
+                                    src="{{ asset('img/avatar.png') }}"
+                                    class="rounded-circle mr-1"
+                                    style="max-width: 175px; max-width: 175px; min-width: 50px; min-width: 50px;">
+                                <form id="form-img" class="mt-4 pt-1">
+                                    <button class="btn btn-danger">Hapus Gambar</button>
+                                </form>
+                            </div>
+                            <div class="col-lg-9 col-md-9 col-12 mb-3">
+                                <form id="form-akun">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}" id="form-token">
+                                    <input type="text" name="userid" id="userid" hidden value="x">
+                                    <div class="form-group row mb-4 d-flex align-item-center justify-content-center">
+                                        <label class="mylabel col-form-label text-md-right col-12 col-md-3 col-lg-3 mt-1">Nama Lengkap</label>
+                                        <div class="col-sm-12 col-md-9">
+                                            <input type="text" class="form-control" name="name" id="name" placeholder="nama lengkap" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4 d-flex align-item-center justify-content-center">
+                                        <label class="mylabel col-form-label text-md-right col-12 col-md-3 col-lg-3 mt-1">Email</label>
+                                        <div class="col-sm-12 col-md-9">
+                                            <input type="email" class="form-control" name="email" id="email" placeholder="email" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4 d-flex align-item-center justify-content-center">
+                                        <label class="mylabel col-form-label text-md-right col-12 col-md-3 col-lg-3 mt-1">Foto profile</label>
+                                        <div class="col-sm-12 col-md-9">
+                                            <input type="file" class="form-control" name="foto" id="foto">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4 pt-1">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <button type="submit" id="btn-update" class="btn btn-primary">Perbarui Data</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <button class="btn btn-icon icon-left btn-success py-2" data-toggle="modal" data-target="#modal-pass"><i class="fas fa-key"></i> Ganti Password</button>
+                        <button class="btn btn-icon icon-left btn-success py-2" data-toggle="modal" data-target="#modal-lupa"><i class="fas fa-unlock-keyhole"></i></i> Fitur Lupa Password</button>
                     </div>
                 </div>
             </div>
         </section>
     </div>
+    @include('profile.modal.reset-pass')
+    @include('profile.modal.lupa-pass')
 @endsection
 
 @push('scripts')
---
+++ resources/views/profile/modal/reset-pass.blade.php
@@ -0,0 +1,34 @@
+<div class="modal fade" tabindex="-1" role="dialog" id="modal-pass">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-primary text-white">
+                <h5 class="modal-title">Reset password</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form id="editForm">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" id="tokenEdit">
+                <div class="modal-body">              
+                    <div class="form-group">
+                        <label>Password Lama</label>
+                        <input type="password" class="form-control" id="oldpassword" name="oldpassword" minlength="5" maxlength="255" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Password Baru</label>
+                        <input type="password" class="form-control" id="newpassword" name="newpassword" minlength="5" maxlength="255" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Konfirmasi Password</label>
+                        <input type="password" class="form-control" id="renewpassword" name="renewpassword" minlength="5" maxlength="255" required>
+                        <span id="edit-rpw-error" class="text-danger"></span>
+                    </div>
+                </div>
+                <div class="modal-footer bg-whitesmoke br">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary" name="btnedit" id="btnedit">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
