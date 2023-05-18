ff39506834b2b64a7e6b78b2dcbc78aa11d484f8
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-05-18 21:25:34 +0700

    setting seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,70 +19,78 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // User::create([
+        //     'name' => 'Dimas Wahyu Ardiyanto',
+        //     'username' => 'dimas',
+        //     'email' => 'dimazwahyuardiyanto@gmail.com',
+        //     'password' => bcrypt('password'),
+        //     'level' => 'pimpinan',
+        //     'status' => 1,
+        // ]);
+        // User::create([
+        //     'name' => 'Aril Arzaqi',
+        //     'username' => 'arzaqi',
+        //     'email' => 'arilarzaqi@gmail.com',
+        //     'password' => bcrypt('password'),
+        //     'level' => 'user',
+        //     'status' => 1,
+        // ]);
+        // Label::create([
+        //     'name' => 'Reparasi',
+        //     'slug' => 'reparasi',
+        //     'jenis' => 0,
+        // ]);
+        // Label::create([
+        //     'name' => 'Jualan',
+        //     'slug' => 'jualan',
+        //     'jenis' => 0,
+        // ]);
+        // Label::create([
+        //     'name' => 'Kas',
+        //     'slug' => 'kas',
+        //     'jenis' => 0,
+        // ]);
+        // Label::create([
+        //     'name' => 'Uang Tips',
+        //     'slug' => 'uang-tips',
+        //     'jenis' => 0,
+        // ]);
+        // Label::create([
+        //     'name' => 'Gaji',
+        //     'slug' => 'gaji',
+        //     'jenis' => 1,
+        // ]);
+        // Label::create([
+        //     'name' => 'Beli Alat dan Bahan',
+        //     'slug' => 'beli-alat-bahan',
+        //     'jenis' => 1,
+        // ]);
+        // Label::create([
+        //     'name' => 'Donasi',
+        //     'slug' => 'donasi',
+        //     'jenis' => 1,
+        // ]);
+        // Label::create([
+        //     'name' => 'Sewa',
+        //     'slug' => 'sewa',
+        //     'jenis' => 1,
+        // ]);
+        // Label::create([
+        //     'name' => 'Pajak',
+        //     'slug' => 'pajak',
+        //     'jenis' => 1,
+        // ]);
+        // User::factory(3)->create();
+        // TMasuk::factory(150)->create();
+        // TKeluar::factory(150)->create();
         User::create([
-            'name' => 'Dimas Wahyu Ardiyanto',
-            'username' => 'dimas',
+            'name' => 'CV Berkah Makmur',
+            'username' => 'berkahmakmurofficial',
             'email' => 'dimazwahyuardiyanto@gmail.com',
             'password' => bcrypt('password'),
             'level' => 'pimpinan',
             'status' => 1,
         ]);
-        User::create([
-            'name' => 'Aril Arzaqi',
-            'username' => 'arzaqi',
-            'email' => 'arilarzaqi@gmail.com',
-            'password' => bcrypt('password'),
-            'level' => 'user',
-            'status' => 1,
-        ]);
-        Label::create([
-            'name' => 'Reparasi',
-            'slug' => 'reparasi',
-            'jenis' => 0,
-        ]);
-        Label::create([
-            'name' => 'Jualan',
-            'slug' => 'jualan',
-            'jenis' => 0,
-        ]);
-        Label::create([
-            'name' => 'Kas',
-            'slug' => 'kas',
-            'jenis' => 0,
-        ]);
-        Label::create([
-            'name' => 'Uang Tips',
-            'slug' => 'uang-tips',
-            'jenis' => 0,
-        ]);
-        Label::create([
-            'name' => 'Gaji',
-            'slug' => 'gaji',
-            'jenis' => 1,
-        ]);
-        Label::create([
-            'name' => 'Beli Alat dan Bahan',
-            'slug' => 'beli-alat-bahan',
-            'jenis' => 1,
-        ]);
-        Label::create([
-            'name' => 'Donasi',
-            'slug' => 'donasi',
-            'jenis' => 1,
-        ]);
-        Label::create([
-            'name' => 'Sewa',
-            'slug' => 'sewa',
-            'jenis' => 1,
-        ]);
-        Label::create([
-            'name' => 'Pajak',
-            'slug' => 'pajak',
-            'jenis' => 1,
-        ]);
-        User::factory(3)->create();
-        TMasuk::factory(150)->create();
-        TKeluar::factory(150)->create();
         Dana::factory(1)->create();
     }
 }
